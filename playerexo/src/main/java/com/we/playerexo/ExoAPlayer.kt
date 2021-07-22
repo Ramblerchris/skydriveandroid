90358cb5a7d8e2305a89747821c56b6c8d63a5bf
wuyishun <wuyishun>
2021-07-22 16:05:21 +0800

    修复播放问题

--- playerexo/src/main/java/com/we/playerexo/ExoAPlayer.kt
+++ playerexo/src/main/java/com/we/playerexo/ExoAPlayer.kt
@@ -7,9 +7,14 @@ import android.view.Surface
 import android.view.SurfaceHolder
 import com.blankj.utilcode.util.LogUtils
 import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.analytics.AnalyticsCollector
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.MediaSource.MediaPeriodId
 import com.google.android.exoplayer2.source.MediaSourceEventListener
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.util.Clock
 import com.google.android.exoplayer2.video.VideoListener
 import com.we.player.player.APlayer
 import com.we.player.player.PlayStatus
@@ -28,8 +33,6 @@ class ExoAPlayer(var app: Application) : APlayer(), Player.EventListener, VideoL
     var simpleExoPlayer: SimpleExoPlayer? = null
     val newInstance = ExoSourceHelper.newInstance(app)
     var parameters: PlaybackParameters? = null
-    private var mLastReportedPlaybackState = PlayStatus.STATE_IDLE
-    private var mLastReportedPlayWhenReady = false
     private var mIsPreparing = false
     private var mIsBuffering = false
     var lastIsMulte: Boolean = false
@@ -44,15 +47,22 @@ class ExoAPlayer(var app: Application) : APlayer(), Player.EventListener, VideoL
     override fun initPlayer() {
         simpleExoPlayer = SimpleExoPlayer.Builder(
                 app.applicationContext,
-                DefaultRenderersFactory(app)
+                DefaultRenderersFactory(app),
+            DefaultTrackSelector(app),
+            DefaultMediaSourceFactory(app),
+            DefaultLoadControl(),
+            DefaultBandwidthMeter.getSingletonInstance(app),
+            AnalyticsCollector(Clock.DEFAULT)
         ).build()
-
+        simpleExoPlayer?.playWhenReady = true
         simpleExoPlayer?.addListener(this)
         simpleExoPlayer?.addVideoListener(this)
     }
 
     override fun setDataSource(path: String?, headers: Map<String, String>?) {
         mMediaSource = newInstance.getMediaSource(path!!, headers)
+        mMediaSource!!.addEventListener(Handler(), mMediaSourceEventListener)
+
     }
 
     override fun setSurface(surface: Surface?) {
@@ -68,9 +78,8 @@ class ExoAPlayer(var app: Application) : APlayer(), Player.EventListener, VideoL
         if (mMediaSource == null) {
             return
         }
-        simpleExoPlayer?.playWhenReady = true
+        mIsPreparing = true
         simpleExoPlayer?.setMediaSource(mMediaSource!!)
-        mMediaSource!!.addEventListener(Handler(), mMediaSourceEventListener)
         simpleExoPlayer?.prepare()
     }
 
@@ -89,6 +98,7 @@ class ExoAPlayer(var app: Application) : APlayer(), Player.EventListener, VideoL
     override fun reset() {
         simpleExoPlayer?.stop()
         simpleExoPlayer?.setVideoSurface(null)
+        mIsPreparing = false
     }
 
     override fun seekTo(seekto: Long) {
@@ -188,18 +198,41 @@ class ExoAPlayer(var app: Application) : APlayer(), Player.EventListener, VideoL
 
     override fun onRenderedFirstFrame() {
         super.onRenderedFirstFrame()
-        if (mPlayerEventListener != null && !mIsPreparing) {
+        if (mPlayerEventListener != null && mIsPreparing) {
+            mPlayerEventListener?.onPlayerEventPrepared()
             mPlayerEventListener?.onPlayerEventInfo(PlayStatus.MEDIA_INFO_VIDEO_RENDERING_START, 0)
-            mIsPreparing = false
         }
     }
 
     override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
         super.onPlayerStateChanged(playWhenReady, playbackState)
         LogUtils.d(TAG, "onPlayerStateChanged playWhenReady：$playWhenReady playbackState：$playbackState")
-
+        if (mPlayerEventListener == null) {
+            return
+        }
+        if (mIsPreparing) {
+            if (playbackState == Player.STATE_READY) {
+                mIsPreparing = false;
+            }
+            return;
+        }
+        when (playbackState) {
+            Player.STATE_BUFFERING -> {
+                mPlayerEventListener?.onPlayerEventInfo(
+                    PlayStatus.MEDIA_INFO_BUFFERING_START,
+                    getBufferedPercentage()
+                )
+            }
+            Player.STATE_READY -> {
+                mPlayerEventListener?.onPlayerEventInfo(
+                    PlayStatus.MEDIA_INFO_BUFFERING_END,
+                    getBufferedPercentage()
+                )
+            }
+            Player.STATE_ENDED -> mPlayerEventListener?.onPlayerEventCompletion()
+        }
 //        if (mIsPreparing) return
-        if (mLastReportedPlayWhenReady != playWhenReady || mLastReportedPlaybackState != playbackState) {
+       /* if (mLastReportedPlayWhenReady != playWhenReady || mLastReportedPlaybackState != playbackState) {
             when (playbackState) {
                 Player.STATE_BUFFERING -> {
                     mPlayerEventListener?.onPlayerEventInfo(PlayStatus.MEDIA_INFO_BUFFERING_START, getBufferedPercentage())
@@ -213,7 +246,7 @@ class ExoAPlayer(var app: Application) : APlayer(), Player.EventListener, VideoL
             }
             mLastReportedPlaybackState = playbackState
             mLastReportedPlayWhenReady = playWhenReady
-        }
+        }*/
     }
 
     override fun onPlayerError(error: ExoPlaybackException) {
