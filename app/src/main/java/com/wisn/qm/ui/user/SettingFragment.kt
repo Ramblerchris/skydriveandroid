b67abf4f428ee34181760b3e1c6d83d085fb03cb
wuyishun <wuyishun>
2021-08-08 16:55:13 +0800

    配置初始化

--- app/src/main/java/com/wisn/qm/ui/user/SettingFragment.kt
+++ app/src/main/java/com/wisn/qm/ui/user/SettingFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
 import com.blankj.utilcode.util.AppUtils
-import com.blankj.utilcode.util.ToastUtils
 import com.library.base.BaseFragment
 import com.library.base.config.GlobalConfig
 import com.library.base.utils.MToastUtils
@@ -22,14 +21,79 @@ import kotlinx.android.synthetic.main.fragment_setting.*
 
 open class SettingFragment : BaseFragment<UserViewModel>(), View.OnClickListener {
     lateinit var title: QMUIQQFaceView
-    val tipRing by lazy { groupListView?.createItemView(null, "上传提示音", null, QMUICommonListItemView.HORIZONTAL, QMUICommonListItemView.ACCESSORY_TYPE_SWITCH) }
-    val tipVibrate by lazy { groupListView?.createItemView(null, "上传提示震动", null, QMUICommonListItemView.HORIZONTAL, QMUICommonListItemView.ACCESSORY_TYPE_SWITCH) }
-    val autoUpload by lazy { groupListView?.createItemView(null, "自动同步", null, QMUICommonListItemView.HORIZONTAL, QMUICommonListItemView.ACCESSORY_TYPE_SWITCH) }
-    val lowBatteryUpload by lazy { groupListView?.createItemView(null, "低电量同步", null, QMUICommonListItemView.HORIZONTAL, QMUICommonListItemView.ACCESSORY_TYPE_SWITCH) }
-    val previewImageOrigin by lazy { groupListView?.createItemView(null, "大图自动加载原图", null, QMUICommonListItemView.HORIZONTAL, QMUICommonListItemView.ACCESSORY_TYPE_SWITCH) }
-    val serverManager by lazy { groupListView?.createItemView(null, "服务器管理", " ", QMUICommonListItemView.HORIZONTAL, QMUICommonListItemView.ACCESSORY_TYPE_CHEVRON) }
-    val versionItem by lazy { groupListView?.createItemView(null, "版本号", AppUtils.getAppVersionName(), QMUICommonListItemView.HORIZONTAL, QMUICommonListItemView.ACCESSORY_TYPE_NONE) }
-    val about by lazy { groupListView?.createItemView(null, "关于APP", " ", QMUICommonListItemView.HORIZONTAL, QMUICommonListItemView.ACCESSORY_TYPE_CHEVRON) }
+    val tipRing by lazy {
+        groupListView?.createItemView(
+            null,
+            "上传提示音",
+            null,
+            QMUICommonListItemView.HORIZONTAL,
+            QMUICommonListItemView.ACCESSORY_TYPE_SWITCH
+        )
+    }
+    val tipVibrate by lazy {
+        groupListView?.createItemView(
+            null,
+            "上传提示震动",
+            null,
+            QMUICommonListItemView.HORIZONTAL,
+            QMUICommonListItemView.ACCESSORY_TYPE_SWITCH
+        )
+    }
+    val autoUpload by lazy {
+        groupListView?.createItemView(
+            null,
+            "自动同步",
+            null,
+            QMUICommonListItemView.HORIZONTAL,
+            QMUICommonListItemView.ACCESSORY_TYPE_SWITCH
+        )
+    }
+    val lowBatteryUpload by lazy {
+        groupListView?.createItemView(
+            null,
+            "低电量同步",
+            null,
+            QMUICommonListItemView.HORIZONTAL,
+            QMUICommonListItemView.ACCESSORY_TYPE_SWITCH
+        )
+    }
+    val previewImageOrigin by lazy {
+        groupListView?.createItemView(
+            null,
+            "大图自动加载原图",
+            null,
+            QMUICommonListItemView.HORIZONTAL,
+            QMUICommonListItemView.ACCESSORY_TYPE_SWITCH
+        )
+    }
+    val serverManager by lazy {
+        groupListView?.createItemView(
+            null,
+            "服务器管理",
+            " ",
+            QMUICommonListItemView.HORIZONTAL,
+            QMUICommonListItemView.ACCESSORY_TYPE_CHEVRON
+        )
+    }
+    val versionItem by lazy {
+        groupListView?.createItemView(
+            null,
+            "版本号",
+            AppUtils.getAppVersionName(),
+            QMUICommonListItemView.HORIZONTAL,
+            QMUICommonListItemView.ACCESSORY_TYPE_NONE
+        )
+    }
+    val about by lazy {
+        groupListView?.createItemView(
+            null,
+            "关于APP",
+            " ",
+            QMUICommonListItemView.HORIZONTAL,
+            QMUICommonListItemView.ACCESSORY_TYPE_CHEVRON
+        )
+    }
+
     override fun layoutId() = R.layout.fragment_setting
     override fun initView(views: View) {
         super.initView(views)
@@ -44,7 +108,6 @@ open class SettingFragment : BaseFragment<UserViewModel>(), View.OnClickListener
         tipRing?.switch?.setOnCheckedChangeListener { _: CompoundButton, b: Boolean ->
             if (b) {
                 tipRing?.setDetailText("开")
-
             } else {
                 tipRing?.setDetailText("关")
             }
@@ -84,22 +147,26 @@ open class SettingFragment : BaseFragment<UserViewModel>(), View.OnClickListener
         }
 
         QMUIGroupListView.newSection(context)
-                .setTitle("")
-                .setDescription("")
-                .setLeftIconSize(QMUIDisplayHelper.dp2px(context, 18), ViewGroup.LayoutParams.WRAP_CONTENT)
-                .addItemView(tipRing, this)
-                .addItemView(tipVibrate, this)
+            .setTitle("")
+            .setDescription("")
+            .setLeftIconSize(
+                QMUIDisplayHelper.dp2px(context, 18),
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            .addItemView(tipRing, this)
+            .addItemView(tipVibrate, this)
 //                .addItemView(autoUpload, this)
-                .addItemView(lowBatteryUpload, this)
-                .addItemView(previewImageOrigin, this)
-                .addItemView(serverManager, this)
-                .addItemView(versionItem, this)
-                .addItemView(about, this)
-                .setMiddleSeparatorInset(QMUIDisplayHelper.dp2px(context, 18), 0)
-                .addTo(groupListView)
+            .addItemView(lowBatteryUpload, this)
+            .addItemView(previewImageOrigin, this)
+            .addItemView(serverManager, this)
+            .addItemView(versionItem, this)
+            .addItemView(about, this)
+            .setMiddleSeparatorInset(QMUIDisplayHelper.dp2px(context, 18), 0)
+            .addTo(groupListView)
         tipRing?.switch?.isChecked = GlobalConfig.tipRing
         tipVibrate?.switch?.isChecked = GlobalConfig.tipVibrate
         lowBatteryUpload?.switch?.isChecked = GlobalConfig.lowBatteryUpload
+        previewImageOrigin?.switch?.isChecked = GlobalConfig.previewImageOrigin
     }
 
     override fun onClick(v: View?) {
@@ -107,13 +174,13 @@ open class SettingFragment : BaseFragment<UserViewModel>(), View.OnClickListener
             MToastUtils.show("当前为最新版本")
         } else if (v == about) {
             MessageDialogBuilder(activity)
-                    .setTitle("关于APP")
-                    .setSkinManager(QMUISkinManager.defaultInstance(context))
-                    .setMessage("专注家庭内部媒体文件同步的app")
-                    .addAction("取消") { dialog, _ ->
-                        dialog.dismiss()
-                    }
-                    .create(R.style.QMUI_Dialog).show()
+                .setTitle("关于APP")
+                .setSkinManager(QMUISkinManager.defaultInstance(context))
+                .setMessage("专注家庭内部媒体文件同步的app")
+                .addAction("取消") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create(R.style.QMUI_Dialog).show()
         } else if (v == serverManager) {
             startFragment(ShutdownServerFragment())
         }
